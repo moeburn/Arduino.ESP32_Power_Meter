5947c6b25dd1b914abaff7add239d673d591c06b
moeburn <moeburn@gmail.com>
2023-12-30 09:21:48 -0500

    This version retrieves data from Arduino over serial

--- Power_ESP32.ino
+++ Power_ESP32.ino
@@ -5,13 +5,19 @@
 #include <ESPAsyncWebServer.h>
 #include <AsyncElegantOTA.h>
 #include "time.h"
+#include <HardwareSerial.h>
 
-#include <Adafruit_ADS1X15.h>
-#include <EmonLib_ADS1x15.h>
+HardwareSerial SerialPort(2); // use UART2
 
-#define ADS (0x48)
 
-EnergyMonitor emon1, emon2;                   // Create an instance
+ const byte numChars = 32;
+char receivedChars[numChars];   // an array to store the received data
+
+boolean newData = false;
+boolean seenData = false;
+
+
+               // Create an instance
 
 const char* ssid = "mikesnet";
 const char* password = "springchicken";
@@ -20,21 +26,27 @@ const char* ntpServer = "pool.ntp.org";
 const long gmtOffset_sec = -18000;  //Replace with your GMT offset (secs)
 const int daylightOffset_sec = 0;   //Replace with your daylight offset (secs)
 int hours, mins, secs;
-double Irms, Irms2, Irmstot;
+double Irms, Irms2, amps1, amps2, Irmstot;
 float power1, power2;
 String power1str = "0";
 String power2str = "0";
 String strArr[2];
 
 char auth[] = "QY8w6digrc2EItvuiO6AXOmhr88Eiwld";
+char remoteAuth2[] = "8_-CN2rm4ki9P3i_NkPhxIbCiKd5RXhK"; //hubert clock auth
 
 AsyncWebServer server(80);
 
 WidgetTerminal terminal(V10);
+WidgetBridge bridge2(V60);
 
 #define every(interval) \
     static uint32_t __every__##interval = millis(); \
     if (millis() - __every__##interval >= interval && (__every__##interval = millis()))
+
+BLYNK_CONNECTED() {
+  bridge2.setAuthToken (remoteAuth2);
+}
 
 BLYNK_WRITE(V10) {
   if (String("help") == param.asStr()) {
@@ -54,7 +66,9 @@ BLYNK_WRITE(V10) {
     if (String("temps") == param.asStr()) {
 
     terminal.print("Irms: ");
-    terminal.println(Irms);
+    terminal.print(power1);
+    terminal.print("Irms2: ");
+    terminal.println(power2);
     
   }
   terminal.flush();
@@ -69,10 +83,68 @@ void printLocalTime() {
   terminal.flush();
 }
 
+void recvWithEndMarker()
+{
+   static byte ndx = 0;
+   char endMarker = '\n';
+   char rc;
+
+   while (SerialPort.available() > 0 && newData == false)
+   {
+      rc = SerialPort.read();
+
+      if (rc != endMarker)
+      {
+         receivedChars[ndx] = rc;
+         ndx++;
+         if (ndx >= numChars)
+         {
+            ndx = numChars - 1;
+         }
+      }
+      else
+      {
+         receivedChars[ndx] = '\0'; // terminate the string
+         ndx = 0;
+         newData = true;
+      }
+   }
+}
+
+void parseData()
+{
+   if (newData == true)
+   {
+      char *strings[3]; // an array of pointers to the pieces of the above array after strtok()
+      char *ptr = NULL; byte index = 0;
+      ptr = strtok(receivedChars, ",");  // delimiters, semicolon
+      while (ptr != NULL)
+      {
+         strings[index] = ptr;
+         index++;
+         ptr = strtok(NULL, ",");
+      }
+      /*
+      // print all the parts   
+      Serial.println("The Pieces separated by strtok()");
+      for (int n = 0; n < index; n++)
+      {
+         Serial.print("piece ");
+         Serial.print(n);
+         Serial.print(" = ");
+         Serial.println(strings[n]);
+      }
+      */
+      // convert string data to numbers
+      power1 = atof(strings[0]) / 100;
+      power2 = atof(strings[1]) / 100;
+      newData = false;
+   }
+}
+
 void setup(void) {
-  Serial.begin(9600);
-  emon1.current(ADS, true, GAIN_ONE, RATE_ADS1115_860SPS, 112.31); //22.1ohm
-  emon2.current(ADS, true, GAIN_ONE, RATE_ADS1115_860SPS, 113.34);       //21.9ohm   // Current : input ads_address, with init ads, Gain, Data rate, calibration.
+   SerialPort.begin(9600, SERIAL_8N1, 16, 17); 
+     //21.9ohm   // Current : input ads_address, with init ads, Gain, Data rate, calibration.
   WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
   Serial.println("");
@@ -116,15 +188,21 @@ void setup(void) {
 
 void loop() {
       Blynk.run();
-       Irms = emon1.calcIrms(0, 1480);  // Calculate Irms only from Single A0 pin. , NUMBER_OF_SAMPLES
-       Irms2 = emon2.calcIrms(1, 1480);
-       Irmstot = Irms + Irms2;
+
+   recvWithEndMarker();
+   parseData();
+
+       Irmstot = power1 + power2;
       // double Irms = emon1.calcIrms(32, 1480);  // Calculate Irms only from Differential A2-A3 pins. NUMBER_OF_SAMPLES
-      every(1000){
-        if (millis() > 20000) {Blynk.virtualWrite(V3, Irmstot);
-        Blynk.virtualWrite(V2, (Irmstot*120.0));}
+      every(2000){
+
+        Blynk.virtualWrite(V3, Irmstot);
+        Blynk.virtualWrite(V2, (Irmstot*120.0));
+        Blynk.virtualWrite(V4, power1);
+        Blynk.virtualWrite(V5, power2);
+        
       }
-
-   
-
+      every(10000){
+        bridge2.virtualWrite(V81, Irmstot);
+      }
 }
